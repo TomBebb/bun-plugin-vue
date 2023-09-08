3903b9e45ab45b3aaaa524a9709963a0cda3b564
Tom Bebbington <tophattedcoder@gmail.com>
2023-09-08 21:35:57 +0100

    Fix Vue SFC style handling

--- src/index.ts
+++ src/index.ts
@@ -17,57 +17,59 @@ const makeId = (length: number) => {
     return result;
 }
 
-let lang : Loader = "js"
- const vuePlugin: BunPlugin = {
-  name: "Vue Loader",
-  setup(build) {
-  build.onLoad({  filter: /.*\.vue/}, async args => {
-    const content = await fs.readFile(args.path, "utf8")
+let lang: Loader = "js"
+const vuePlugin: BunPlugin = {
+    name: "Vue Loader",
+    setup(build) {
+        build.onLoad({ filter: /.*\.vue/ }, async args => {
+            const content = await fs.readFile(args.path, "utf8")
 
-    const parsed = parse(content, {filename: args.path})
-    const id =makeId(5);
-    const ssr = false 
-    console.log("Compile stlye")
-    const style = await compileStyleAsync({
-        filename: args.path,
-        source: content,
-        id
-    })  
-    console.log("Compiled style", style)
+            const parsed = parse(content, { filename: args.path })
+            const id = makeId(5);
+            const ssr = false
+            console.log("Compile stlye")
+
+            let style = ""
+            await Promise.all(parsed.descriptor.styles.map(async s => {
+                const comp = await compileStyleAsync({
+                    filename: args.path,
+                    source: s.content,
+                    scoped: s.scoped,
+                    id
+                })
+                style += comp.code
+
+            }))
 
 
-    let isProd = process.env.NODE_ENV == "development" ? false : true;
-    console.log('parsed')
-        
-    let code = ""
-    const scrSetup = parsed.descriptor.scriptSetup
-    if (scrSetup?.setup) {
-        console.log('attrs', scrSetup?.attrs)
-        if(scrSetup?.attrs?.lang === "ts") {
-            lang = "ts"
-        }
-        // vue setup
-        let { content: code2 } = compileScript(parsed.descriptor, {
-            id,
-            inlineTemplate: true, // SSR friendly
-            isProd, 
-            templateOptions: {
-                filename: basename(args.path),
-                ssr,
-                ssrCssVars: []
+            let isProd = process.env.NODE_ENV == "development" ? false : true;
+            const scrSetup = parsed.descriptor.scriptSetup
+            if (scrSetup?.setup) {
+                //console.log('attrs', scrSetup?.attrs)
+                if (scrSetup?.attrs?.lang === "ts") {
+                    lang = "ts"
+                }
+                // vue setup
+                let { content: code2 } = compileScript(parsed.descriptor, {
+                    id,
+                    inlineTemplate: true, // SSR friendly
+                    isProd,
+                    templateOptions: {
+                        filename: basename(args.path),
+                        ssr,
+                        ssrCssVars: []
+                    }
+                });
+                code += code2;
             }
-        });
-        code += code2;
-    }
 
 
-    console.log(code ,lang)
-    return {
-      loader: lang,
+            return {
+                loader: lang,
 
-      contents: `${code};const s=document.createElement("style");s.innerText=${JSON.stringify(style.code)};document.onload=()=>document.body.appendChild(s)`,
-    }
-  })
-  },
+                contents: `${code};const s=document.createElement("style");s.innerText='todo';document.onload=()=>document.body.appendChild(s)`,
+            }
+        })
+    },
 }
 export default vuePlugin
